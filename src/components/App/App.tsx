2641ea450ba30862f3104cba489bd563aad9a302
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-07 13:35:39 +0300

    feat: Add Footer.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import stylesApp from "./App.module.css";
 import { AuthForm } from "./AuthForm/AuthForm";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <div className={stylesApp.page}>
-      <header></header>
+      <Header />
       <main>
         <AuthForm />
       </main>
-      <footer></footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/App/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import stylesFooter from "./Footer.module.css";
+
+function Footer() {
+  return <footer></footer>;
+}
+
+export default Footer;
