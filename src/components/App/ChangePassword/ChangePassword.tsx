8bd70c378278242914be4d6b5187f24390aa82cd
Aleksandr Demidenko <sashatro@mail.ru>
2023-02-08 01:31:23 +0300

    feat: Add input styles

--- src/components/App/ChangePassword/ChangePassword.tsx
+++ src/components/App/ChangePassword/ChangePassword.tsx
@@ -22,68 +22,79 @@ function ChangePassword() {
         />
         <h1 className={stylesChangePasswordForm.title}>Изменение пароля</h1>
         <fieldset className={stylesChangePasswordForm.field}>
-          <input
-            type="text"
-            id="login"
-            name="login"
-            minLength={1}
-            // value={nameRU}
-            // onChange={onTitleChanged}
-            className={stylesChangePasswordForm.input}
-            placeholder="Email / Логин"
-            required
-          />
+          <label htmlFor="login" className={stylesChangePasswordForm.label}>
+            <input
+              type="text"
+              id="login"
+              name="login"
+              minLength={1}
+              // value={nameRU}
+              // onChange={onTitleChanged}
+              className={stylesChangePasswordForm.input}
+              placeholder="Email / Логин"
+              required
+            />
+          </label>
           <span className={stylesChangePasswordForm.error}>
             Поле должно быть заполнено
           </span>
         </fieldset>
 
         <fieldset className={stylesChangePasswordForm.field}>
-          <input
-            type="text"
-            id="control"
-            name="control"
-            minLength={1}
-            // value={nameRU}
-            // onChange={onTitleChanged}
-            className={stylesChangePasswordForm.input}
-            placeholder="Контрольная строка"
-            required
-          />
+          <label htmlFor="control" className={stylesChangePasswordForm.label}>
+            <input
+              type="text"
+              id="control"
+              name="control"
+              minLength={1}
+              // value={nameRU}
+              // onChange={onTitleChanged}
+              className={stylesChangePasswordForm.input}
+              placeholder="Контрольная строка"
+              required
+            />
+          </label>
           <span className={stylesChangePasswordForm.error}>
             Неверное контрольное слово
           </span>
         </fieldset>
 
         <fieldset className={stylesChangePasswordForm.field}>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            minLength={6}
-            // value={nameRU}
-            // onChange={onTitleChanged}
-            className={stylesChangePasswordForm.input}
-            placeholder="Новый пароль"
-            required
-          />
+          <label htmlFor="password" className={stylesChangePasswordForm.label}>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              minLength={6}
+              // value={nameRU}
+              // onChange={onTitleChanged}
+              className={stylesChangePasswordForm.input}
+              placeholder="Новый пароль"
+              required
+            />
+          </label>
           <span className={stylesChangePasswordForm.error}>
             Пароль должен быть не менее 6 символов
           </span>
         </fieldset>
 
         <fieldset className={stylesChangePasswordForm.field}>
-          <input
-            type="password"
-            id="confirmation"
-            name="confirmation"
-            minLength={6}
-            // value={nameRU}
-            // onChange={onTitleChanged}
-            className={stylesChangePasswordForm.input}
-            placeholder="Подтверждение пароля"
-            required
-          />
+          <label
+            htmlFor="confirmation"
+            className={stylesChangePasswordForm.label}
+          >
+            <input
+              type="password"
+              id="confirmation"
+              name="confirmation"
+              minLength={6}
+              // value={nameRU}
+              // onChange={onTitleChanged}
+              className={stylesChangePasswordForm.input}
+              placeholder="Подтверждение пароля"
+              required
+            />
+          </label>
           <span className={stylesChangePasswordForm.error}>
             Пароли не совпадают
           </span>
